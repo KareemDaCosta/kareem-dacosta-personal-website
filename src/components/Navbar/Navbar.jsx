ef202d5d848af6ee48abc35c00565806b08b9548
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2022-09-01 15:26:24 -0400

    Implemented Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import './Navbar.css';
+import { Link } from "react-router-dom";
+
 
 export default function Navbar() {
     return (
         <div className="navbar">
-            
+            <Link to={'/'}><h1>Kareem DaCosta</h1></Link>
+				<div class="navbar-button-container">
+					<Link to={`/experience`} class="experience-button navbar-button">EXPERIENCE</Link>
+					<Link to={`/projects`} class="projects-button navbar-button">PROJECTS</Link>
+					<Link to={`/education`} class="education-button navbar-button">EDUCATION</Link>
+                    <Link to={`/skills`} class="skills-button navbar-button">SKILLS</Link>
+				</div>
         </div>
     )
 }

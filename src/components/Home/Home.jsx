f2851afe6e0ed86feee5cd105731f00d6ae8367b
Kareem DaCosta <90730490+KareemDaCosta@users.noreply.github.com>
2023-03-09 15:34:06 -0500

    Updated Homepage description

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -29,7 +29,7 @@ export default function Home() {
               </div>
             </div>
             <div className="profile-card-text-blurb">
-              A Black/Egyptian second-year student at Columbia University's School of
+              A second-year student at Columbia University's School of
               Engineering and Applied Sciences majoring in computer science.
             </div>
           </div>
